b6b6e72c27ddb2684079bb403c8478bf015c0668
essraaamr2002 <77300591+essraaamr2002@users.noreply.github.com>
2024-05-01 15:35:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 var typed = new Typed(".text", {
-    strings: ["Programming" , "Cybersecurity" , "Web Development"],
+    strings: ["Programming" , "Front End" , "Web Development"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
